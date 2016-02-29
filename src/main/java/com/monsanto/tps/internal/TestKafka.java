b3565b7ac3ca9e3cf29ebe495047c02b43207693
SMALA <james!123>
2016-02-29 18:13:28 +0530

    Kafka setup - java

---
+++ src/main/java/com/monsanto/tps/internal/TestKafka.java
@@ -0,0 +1,28 @@
+package com.monsanto.tps.internal;
+
+/**
+ * Created by SMALA on 2/29/2016.
+ */
+public class TestKafka {
+
+    /*private static Producer<Integer, String> producer;
+    private final Properties props = new Properties();
+    public SimpleProducer()
+    {
+        props.put("broker.list", "localhost:9092");
+        props.put("serializer.class", "kafka.serializer.StringEncoder");
+        props.put("request.required.acks", "1");
+        producer = new Producer<Integer, String>(new
+                ProducerConfig(props));
+    }
+
+    public static void main(String[] args) {
+        SimpleProducer sp = new SimpleProducer();
+        String topic = (String) args[0];
+        String messageStr = (String) args[1];
+        KeyedMessage<Integer, String> data = new KeyedMessage<Integer,
+                String>(topic, messageStr);
+        producer.send(data);
+        producer.close();
+    }*/
+}
